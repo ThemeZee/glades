b11dbc7b3a53d08747a0d1cefe204d6c5778c33d
Thomas W <netzberufler@googlemail.com>
2016-01-19 18:11:20 +0100

    updated upgrade control and made themezee.com links translateable

--- inc/customizer/functions/custom-controls.php
+++ inc/customizer/functions/custom-controls.php
@@ -42,50 +42,34 @@ if ( class_exists( 'WP_Customize_Control' ) ) :
 				<span class="customize-control-title"><?php esc_html_e( 'Pro Version', 'glades' ); ?></span>
 				
 				<span class="textfield">
-					<?php printf( esc_html__( 'Purchase the Pro Version of %s to get additional features and advanced customization options.', 'glades' ), 'Glades'); ?>
+					<?php printf( esc_html__( 'Purchase the Pro Version of %s to get additional features and advanced customization options.', 'glades' ), 'Anderson'); ?>
 				</span>
 				
 				<p>
-					<a href="https://themezee.com/themes/glades/?utm_source=customizer&utm_medium=button&utm_campaign=glades&utm_content=pro-version" target="_blank" class="button button-secondary">
+					<a href="<?php echo esc_url( __( 'https://themezee.com/addons/glades-pro/', 'glades' ) ); ?>?utm_source=customizer&utm_medium=button&utm_campaign=glades&utm_content=pro-version" target="_blank" class="button button-secondary">
 						<?php printf( esc_html__( 'Learn more about %s Pro', 'glades' ), 'Glades'); ?>
 					</a>
 				</p>
 				
 			</div>
 			
-			<div class="upgrade-toolkit">
+			<div class="upgrade-plugins">
 			
-				<span class="customize-control-title"><?php esc_html_e( 'ThemeZee Toolkit', 'glades' ); ?></span>
+				<span class="customize-control-title"><?php esc_html_e( 'ThemeZee Plugins', 'glades' ); ?></span>
 				
 				<span class="textfield">
-					<?php esc_html_e( 'The ThemeZee Toolkit add-on is a collection of useful small modules and features, neatly bundled into a single plugin.', 'glades' ); ?>
+					<?php esc_html_e( 'Extend the functionality of your WordPress website with our customized plugins.', 'glades' ); ?>
 				</span>
 				
 				<p>
-					<a href="https://themezee.com/plugins/toolkit/?utm_source=customizer&utm_medium=button&utm_campaign=glades&utm_content=toolkit" target="_blank" class="button button-secondary">
-						<?php printf( esc_html__( 'View Details', 'glades' ), 'Glades'); ?>
+					<a href="<?php echo esc_url( __( 'https://themezee.com/plugins/', 'glades' ) ); ?>?utm_source=customizer&utm_medium=button&utm_campaign=glades&utm_content=plugins" target="_blank" class="button button-secondary">
+						<?php esc_html_e( 'Browse Plugins', 'glades' ); ?>
 					</a>
 					<a href="<?php echo admin_url( 'plugin-install.php?tab=search&type=author&s=themezee' ); ?>" class="button button-primary">
 						<?php esc_html_e( 'Install now', 'glades' ); ?>
 					</a>
 				</p>
 			
-			</div>
-			
-			<div class="upgrade-addons">
-			
-				<span class="customize-control-title"><?php esc_html_e( 'Add-on plugins', 'glades' ); ?></span>
-				
-				<span class="textfield">
-					<?php esc_html_e( 'Extend the functionality of your WordPress website with our customized add-ons.', 'glades' ); ?>
-				</span>
-
-				<p>
-					<a href="https://themezee.com/plugins/?utm_source=customizer&utm_medium=button&utm_campaign=glades&utm_content=addons" target="_blank" class="button button-secondary">
-						<?php esc_html_e( 'Browse our add-ons', 'glades' ); ?>
-					</a>
-				</p>
-				
 			</div>
 			
 			<?php
