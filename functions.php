5d2cf3fd20c9f82a7cab165385bf8811ea4ad067
Thomas W <netzberufler@googlemail.com>
2016-04-13 13:10:11 +0200

    completed custom logo feature

--- functions.php
+++ functions.php
@@ -83,7 +83,7 @@ function glades_setup() {
 	// Set up the WordPress core custom logo feature
 	add_theme_support( 'custom-logo', apply_filters( 'glades_custom_logo_args', array(
 		'height' => 50,
-		'width' => 350,
+		'width' => 300,
 		'flex-height' => true,
 		'flex-width' => true,
 	) ) );
@@ -138,7 +138,7 @@ function glades_register_sidebars() {
 	register_sidebar( array(
 		'name' => esc_html__( 'Sidebar', 'glades' ),
 		'id' => 'sidebar',
-		'description' => esc_html__( 'Appears on posts and pages except Magazine Homepage and Fullwidth template.', 'glades' ),
+		'description' => esc_html__( 'Appears on posts and pages except the full width template.', 'glades' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s clearfix">',
 		'after_widget' => '</aside>',
 		'before_title' => '<h3 class="widgettitle"><span>',
